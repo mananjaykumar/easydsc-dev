2b218aec72196b5baad78a5958136c478c3b8920
mananjaykumar <sinharishab100@gmail.com>
2023-01-06 00:12:12 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
         <h1>Welcome to Udyam Registration.</h1>
       </header>
     </div>
