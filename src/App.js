c0581ef0ad9310299ba9373a4fd1fd30d38ca7bd
mananjaykumar <63443042+mananjaykumar@users.noreply.github.com>
2023-01-05 18:10:17 +0530

    checking 4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} alt="logo" />
         <h1>Welcome to Udyam Registration</h1>       
       </header>
     </div>
