408c294272488cd34a548aaa35d7284b2d6dae19
mananjaykumar <sinharishab100@gmail.com>
2023-01-05 23:42:10 +0530

    initial commit

--- lint-staged.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    '*.{js,jsx,ts,tsx}': [
-        // 'eslint --max-warnings=1', 
-        'npm run lint', 
-        () => 'tsc-files --noEmit',
-    ],
-    '*.{js,jsx,ts,tsx,json,css,js}': ['prettier --write'],
-}
