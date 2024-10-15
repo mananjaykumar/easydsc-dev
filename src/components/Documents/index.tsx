db9c918c1ae833bd53cdb633c6550d43376b0bae
mananjaykumar <sinharishab100@gmail.com>
2024-10-15 19:33:57 +0530

    document now open in another tab and in browser

--- src/components/Documents/index.tsx
+++ src/components/Documents/index.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable  @typescript-eslint/no-unused-vars */
 import React, { useEffect, useState } from 'react';
 import { Button, CircularProgress, Grid, Stack, Typography } from '@mui/material';
 import DocViewer, {
